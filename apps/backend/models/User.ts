2a62a3e978338e5e75b56dde954438abaca0a5ba
heywowyou <pro@julesfontaine.com>
2025-04-24 19:44:55 +0200

    Create bare bones user schema

---
+++ apps/backend/models/User.ts
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
