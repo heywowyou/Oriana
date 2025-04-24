9930f02c1d6741eaca9472e677ddf3bc09f1be4b
heywowyou <pro@julesfontaine.com>
2025-04-24 19:46:09 +0200

    Add email to user schema

--- apps/backend/models/User.ts
+++ apps/backend/models/User.ts
@@ -3,7 +3,8 @@ import mongoose, { Schema } from "mongoose";
 const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true, trim: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true }, // we can hash it later
   },
   {
     timestamps: true,
