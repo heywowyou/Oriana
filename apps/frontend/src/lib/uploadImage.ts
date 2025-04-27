7eacb6cfd1583e0b7ab942e5c127783abd377b83
heywowyou <pro@julesfontaine.com>
2025-04-27 11:59:35 +0200

    Update upload image function to randomize the image name instead of using date now to avoid duplicate

--- apps/frontend/src/lib/uploadImage.ts
+++ apps/frontend/src/lib/uploadImage.ts
@@ -1,8 +1,21 @@
 import { storage } from "./firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
+function generateRandomId(length: number) {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
 export async function uploadImage(file: File): Promise<string> {
-  const fileRef = ref(storage, `watched-covers/${Date.now()}-${file.name}`);
+  const randomId = generateRandomId(20);
+  const extension = file.name.split(".").pop(); // get file extension like jpg, png
+  const fileRef = ref(storage, `watched-covers/${randomId}.${extension}`);
+
   await uploadBytes(fileRef, file);
   const downloadURL = await getDownloadURL(fileRef);
   return downloadURL;
