74e29d5d0421594c8d9f15cbe4ba4db7960b3a35
heywowyou <pro@julesfontaine.com>
2025-04-27 16:29:10 +0200

    Add auto refresh for firebase token to avoid crash

--- apps/frontend/src/context/AuthContext.tsx
+++ apps/frontend/src/context/AuthContext.tsx
@@ -7,11 +7,14 @@ import {
   useEffect,
   ReactNode,
 } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "@/lib/firebase";
 
 interface AuthContextType {
   idToken: string | null;
   login: (token: string) => void;
   logout: () => void;
+  refreshIdToken: () => Promise<void>;
   isLoggedIn: boolean;
   loading: boolean;
 }
@@ -20,7 +23,6 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState(true);
-
   const [idToken, setIdToken] = useState<string | null>(() => {
     if (typeof window !== "undefined") {
       return localStorage.getItem("idToken");
@@ -38,18 +40,30 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
   const login = (token: string) => {
     setIdToken(token);
-    localStorage.setItem("idToken", token); // Save token to localStorage
+    localStorage.setItem("idToken", token);
   };
 
   const logout = () => {
     setIdToken(null);
-    localStorage.removeItem("idToken"); // Clear token from localStorage
+    localStorage.removeItem("idToken");
+  };
+
+  // RefreshIdToken function
+  const refreshIdToken = async () => {
+    const auth = getAuth(app);
+    const user = auth.currentUser;
+    if (user) {
+      const token = await user.getIdToken(true); // Force refresh
+      setIdToken(token);
+      localStorage.setItem("idToken", token); // Update localStorage
+    }
   };
 
   const value = {
     idToken,
     login,
     logout,
+    refreshIdToken,
     isLoggedIn: !!idToken,
     loading,
   };
--- apps/frontend/src/lib/firebase.ts
+++ apps/frontend/src/lib/firebase.ts
@@ -7,12 +7,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyDhuiTXxMdjzKSbcZGWRF1HooVfllZ2xD8",
   authDomain: "oriana-308063.firebaseapp.com",
   projectId: "oriana-308063",
-  storageBucket: "oriana-308063.firebasestorage.app",
+  storageBucket: "oriana-308063.appspot.com",
   messagingSenderId: "735013743426",
   appId: "1:735013743426:web:e8230a89d2b11e375e76db",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
