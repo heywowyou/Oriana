6887444f6428b619d6363378d3f709a50d250012
heywowyou <pro@julesfontaine.com>
2025-05-01 15:59:52 +0200

    Edit home page message

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -2,8 +2,8 @@
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-6">Welcome back!</h1>
+    <div className="flex justify-center items-center h-1/2">
+      <h1 className="text-2xl font-bold text-zinc-200">Welcome back</h1>
     </div>
   );
 };
